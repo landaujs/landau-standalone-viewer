8b1dab251817289654704950b6078abdb8d4721e
Maximilian Goisser <goisser94@gmail.com>
2017-11-19 10:31:49 +0100

    first steps; mostly on par with atom-viewer

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,15 +1,343 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.css';
+import React3 from 'react-three-renderer';
+import * as THREE from 'three';
+import _ from 'lodash-es';
+import { Treebeard, decorators as treeDecorators } from 'react-treebeard';
+import Mousetrap from 'mousetrap';
 
-export default class Home extends Component {
-  render() {
+import { convertFromCsg, renderedFromPackager, treeFromPackager } from './landau_helper';
+
+const OrbitControls = require('three-orbit-controls')(THREE);
+
+type TreePos = Array<number>;
+
+type Props = {
+};
+
+type State = {
+  hoveredChildPos: ?TreePos,
+  selectedPos: ?TreePos,
+  collapsedChildPos: Array<TreePos>,
+};
+
+export default class Home extends Component<Props, State> {
+  state = {
+    cubeRotation: new THREE.Euler(),
+    modulePath: '/Users/hobofan/stuff/snappy-reprap/landau/scratchpad3.js',
+    mainRendered: null,
+    treebeardData: null,
+    renderedChildren: {},
+    // tree view
+    hoveredChildPos: null,
+    selectedPos: [],
+    collapsedChildPos: [],
+  }
+  cameraPosition = new THREE.Vector3(25, 0, 25);
+
+  componentDidMount() {
+    this.reloadModule();
+
+    Mousetrap.bind('alt+r', () => {
+      this.reloadModule();
+    });
+  }
+
+  reloadModule = () => {
+    this.setState({
+      renderedChildren: {},
+      hoveredChildPos: null,
+      selectedPos: [],
+      collapsedChildPos: [],
+    });
+    this.requestRendered();
+    this.requestTree();
+  }
+
+  requestRendered = () => {
+    const { modulePath, selectedPos } = this.state;
+    const mainOpts = { module_path: modulePath, pos: selectedPos };
+    this.setState({ mainRendered: null });
+    renderedFromPackager(mainOpts, (res) => {
+      const firstElement = convertFromCsg(res[0]);
+      this.setState({
+        mainRendered: {
+          vertices: firstElement.geometry.vertices,
+          faces: firstElement.geometry.faces,
+        },
+      });
+    });
+  }
+
+  requestTree = () => {
+    const { modulePath, selectedPos } = this.state;
+    const mainOpts = { module_path: modulePath, pos: selectedPos };
+    this.setState({ treebeardData: {} });
+    treeFromPackager(mainOpts, (res) => {
+      const treebeardData = this.transformTreeToTreebeard(res);
+      this.setState({
+        treebeardData,
+      }, () => this.prefetchChildren());
+    });
+  }
+
+  allTreePositions = () => {
+    const { treebeardData } = this.state;
+    const positions = [];
+    positions.push([]); // root position
+    const traverse = (obj, i, parentTreePos) => {
+      let treePos = (parentTreePos || []);
+      if (typeof i !== 'undefined') {
+        treePos = treePos.concat([i]);
+      }
+      (obj.children || []).map((obj, i) => traverse(obj, i, treePos));
+      positions.push(treePos);
+    };
+    traverse(treebeardData);
+    return positions;
+  }
+
+  prefetchChildren = () => {
+    const { modulePath } = this.state;
+    const allTreePos = this.allTreePositions();
+    allTreePos.forEach((treePos) => {
+      const mainOpts = { module_path: modulePath, pos: treePos };
+      renderedFromPackager(mainOpts, (res) => {
+        const firstElement = convertFromCsg(res[0]);
+        const renderedChild = {};
+        renderedChild[treePos] = {
+          vertices: firstElement.geometry.vertices,
+          faces: firstElement.geometry.faces,
+        };
+        this.setState({ renderedChildren: {
+          ...this.state.renderedChildren,
+          ...renderedChild,
+        }});
+      });
+    })
+  }
+
+  transformTreeToTreebeard = (tree) => {
+    const mapObject = (obj, i, parentTreePos) => {
+      let treePos = (parentTreePos || []);
+      if (typeof i !== 'undefined') {
+        treePos = treePos.concat([i]);
+      }
+      const name = obj.elementName;
+      const props = obj.props;
+      const children = (obj.children || []).map((obj, i) => mapObject(obj, i, treePos));
+      return {
+        name,
+        children,
+        props,
+        treePos,
+        toggled: true,
+      };
+    }
+    return mapObject(tree);
+  }
+
+  onCameraRef = (camera) => {
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    new OrbitControls(camera);
+  }
+
+  onTreeNodeClick = (node: { treePos: TreePos }) => {
+    this.setState({
+      selectedPos: node.treePos,
+    }, () => {
+      this.requestRendered();
+    });
+  }
+
+  onTreeNodeToggle = (nodePos: TreePos) => {
+    this.setState({
+      collapsedChildPos: _.xor(this.state.collapsedChildPos, [nodePos]),
+    });
+  }
+
+  onTreeNodeHover = (nodePos: TreePos) => {
+    if (nodePos !== this.state.hoveredChildPos) {
+      this.setState({
+        hoveredChildPos: nodePos,
+      });
+    }
+  }
+
+  onTreeNodeHoverLeave = (nodePos: TreePos) => {
+    if (nodePos === this.state.hoveredChildPos) {
+      this.setState({
+        hoveredChildPos: null,
+      });
+    }
+  }
+
+  treeData = () => {
+    let data = this.state.treebeardData || {};
+    data = _.clone(data);
+
+    const { selectedPos } = this.state;
+    const makeModifier = (modifier) => {
+      const modify = (obj, remainingPos) => {
+        if (_.isEmpty(remainingPos)) {
+          return modifier(obj);
+        }
+
+        const children = obj.children.map((child, i) => {
+          const [j, ...childRemainingPos] = remainingPos;
+          if (i === j) {
+            return modify(child, childRemainingPos);
+          }
+          return child;
+        });
+        return {
+          ...obj,
+          children,
+        };
+      };
+      return modify;
+    };
+
+    const markSelected = makeModifier((obj) => {
+      const newObj = _.clone(obj);
+      newObj.active = true;
+      return newObj;
+    });
+    data = markSelected(data, selectedPos);
+
+    const markCollapsed = makeModifier((obj) => {
+      const newObj = _.clone(obj);
+      newObj.toggled = false;
+      return newObj;
+    });
+    this.state.collapsedChildPos.forEach((collapsedPos) => {
+      data = markCollapsed(data, collapsedPos);
+    });
+
+    return data;
+  }
+
+  renderTreeview = () => {
+    const self = this;
+    const decorators = Object.assign({}, treeDecorators, {
+      Toggle: (props) => {
+        return (
+          <div style={props.style.base} onClick={() => this.onTreeNodeToggle(props.node.treePos)}>
+            <treeDecorators.Toggle {...props} />
+          </div>
+        );
+      },
+    });
+    class Container extends treeDecorators.Container {
+      renderToggleDecorator() {
+        const {style, decorators} = this.props;
+
+        return <decorators.Toggle {...this.props} style={style.toggle}/>;
+      }
+    }
+    class TwiceContainer extends React.Component {
+      render() {
+        return (
+          <div
+            onMouseOver={() => self.onTreeNodeHover(this.props.node.treePos)}
+            onMouseLeave={() => self.onTreeNodeHoverLeave(this.props.node.treePos)}
+          >
+            <Container {...this.props} />
+          </div>
+        );
+      }
+    }
+    decorators.Container = TwiceContainer;
+
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+      <Treebeard
+        data={this.treeData()}
+        decorators={decorators}
+        onToggle={this.onTreeNodeClick}
+      />
+    );
+  }
+
+  render() {
+    const { mainRendered, hoveredChildPos } = this.state;
+    const width = 1000;
+    const height = 500;
+
+    const hoveredChild = this.state.renderedChildren[hoveredChildPos];
+
+    return (
+      <div >
+        <React3
+          mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
+          width={width}
+          height={height}
+          antialias
+
+          clearColor={0xcccccc}
+          shadowMapEnabled
+          shadowMapType={THREE.PCFSoftShadowMap}
+        >
+          <scene>
+            <perspectiveCamera
+              ref={this.onCameraRef}
+              name="camera"
+              fov={75}
+              aspect={width / height}
+              near={0.1}
+              far={1000}
+
+              position={this.cameraPosition}
+            />
+            <ambientLight />
+            <pointLight
+              castShadow
+              intensity={1}
+              decay={2}
+              color={0xffffff}
+              position={new THREE.Vector3(25, 25, 25)}
+            />
+            <axisHelper size={10000} />
+            <gridHelper size={100} step={100} />
+            { mainRendered ? (
+              <group>
+                <mesh
+                  castShadow
+                  receiveShadow
+                >
+                  <geometry
+                    vertices={mainRendered.vertices}
+                    faces={mainRendered.faces}
+                    dynamic
+                  />
+                  <meshPhongMaterial
+                    color={0x64b5f6}
+                  />
+                </mesh>
+              </group>
+              ) : null }
+            { hoveredChild ? (
+              <group>
+                <mesh
+                  castShadow
+                  receiveShadow
+                >
+                  <geometry
+                    vertices={hoveredChild.vertices}
+                    faces={hoveredChild.faces}
+                    dynamic
+                  />
+                  <meshPhongMaterial
+                    transparent
+                    opacity={0.5}
+                    color={0xffee58}
+                  />
+                </mesh>
+              </group>
+              ) : null }
+          </scene>
+        </React3>
+        <div style={{ display: 'inline-block', width: '100%', top: 0, position: 'absolute' }}>
+          { this.renderTreeview() }
         </div>
       </div>
     );
---
+++ app/components/landau_helper.js
@@ -0,0 +1,179 @@
+import * as THREE from 'three';
+import { CSG } from '@jscad/csg';
+
+const exec = require('child_process').exec;
+
+// const packagerUrl = atom.config.get('landau-viewer.packagerUrl');
+const packagerUrl = 'http://localhost:1938';
+
+const execute = (command, callback) => {
+  exec(command, (error, stdout, stderr) => {
+    callback(stdout);
+    if (stderr) {
+      console.error(stderr);
+    }
+  });
+};
+
+const renderCommand = (options) => {
+  const command = "curl --request POST \
+                      --url PACKAGER_URL/render \
+                      --header 'content-type: application/json' \
+                      --data 'OPTIONS'"
+      .replace('OPTIONS', JSON.stringify(options))
+      .replace('PACKAGER_URL', packagerUrl);
+  return command;
+};
+
+
+const treeCommand = (options) => {
+  var command = "curl --request POST \
+                          --url PACKAGER_URL/tree \
+                          --header 'content-type: application/json' \
+                          --data 'OPTIONS'"
+      .replace('OPTIONS', JSON.stringify(options))
+      .replace('PACKAGER_URL', packagerUrl);
+  return command;
+};
+
+const jsonToCSG = (json) => CSG.fromPolygons(json.polygons);
+
+const getGeometryVertex = (geometry, vertex_position) => {
+  geometry.vertices.push(new THREE.Vector3(vertex_position._x, vertex_position._y, vertex_position._z));
+  return geometry.vertices.length - 1;
+};
+
+const fromCsg = (csg, defaultColor) => {
+  let i,
+    j,
+    vertices,
+    face,
+    three_geometry = new THREE.Geometry(),
+    polygons = csg.toPolygons();
+  const faces = [];
+
+  // dict of different THREE.Colors in mesh
+  const colors = {};
+  // list of different opacities used by polygons
+  const opacities = [];
+  let materialIdx,
+    opacity,
+    colorKey,
+    polyColor,
+    color;
+
+  polygons.forEach((polygon) => {
+    // polygon shared null? -> defaultColor, else extract color
+    const vertices = polygon.vertices.map((vertex) => getGeometryVertex(three_geometry, vertex.pos));
+
+    if (vertices[0] === vertices[vertices.length - 1]) {
+      vertices.pop();
+    }
+
+    polyColor = polygon.shared.color ?
+      polygon.shared.color.slice() :
+      defaultColor.slice();
+    opacity = polyColor.pop();
+
+    // one material per opacity (color not relevant)
+    // collect different opacity values in opacities
+    // point to current polygon opacity with materialIdx
+    const opacityIdx = opacities.indexOf(opacity);
+    if (opacityIdx > -1) {
+      materialIdx = opacityIdx;
+    } else {
+      opacities.push(opacity);
+      materialIdx = opacities.length - 1;
+    }
+
+    // for each different color, create a color object
+    const colorKey = polyColor.join('_');
+    if (!(colorKey in colors)) {
+      color = new THREE.Color();
+      color.setRGB(...polyColor);
+      colors[colorKey] = color;
+    }
+
+    // create a mesh face using color (not opacity~material)
+    for (let k = 2; k < vertices.length; k++) {
+      const pNormal = polygon.plane.normal;
+      const normal = new THREE.Vector3(pNormal._x, pNormal._y, pNormal._z);
+      face = new THREE.Face3(vertices[0], vertices[k - 1], vertices[k],
+        normal
+      );
+        // colors[colorKey], materialIdx);
+      // face.materialIdx = materialIdx;
+      three_geometry.faces.push(face);
+    }
+  });
+
+  // three_geometry.computeFaceNormals();
+  // three_geometry.computeVertexNormals();
+
+  // pass back bounding sphere radius (or 0 if empty object)
+  // three_geometry.computeBoundingSphere();
+  // var boundLen = three_geometry.boundingSphere.radius +
+  //   three_geometry.boundingSphere.center.length() || 0;
+
+  // return result;
+  return {
+    geometry: three_geometry,
+  };
+};
+
+const convertFromCsg = (obj) => {
+  const options = {
+    faceColor: {
+      r: 0.1,
+      g: 0.7,
+      b: 0.5,
+      a: 1.0
+    }, // default face color
+  };
+  const faceColor = options.faceColor;
+  const defaultColor_ = [faceColor.r, faceColor.g, faceColor.b, faceColor.a || 1];
+  const res = fromCsg(obj, defaultColor_);
+
+  return res;
+};
+
+const renderedFromPackager = (mainOpts, cb) => {
+  const getRendered = (optionsPackets, callback) => {
+    // var converted = [];
+    function recurse(finished, remaining) {
+      if (remaining.length === 0) {
+        callback(finished);
+      } else {
+        execute(renderCommand(remaining.shift()), (mainJson) => {
+          const cvrt = jsonToCSG(JSON.parse(mainJson.trim()));
+
+          finished.push(cvrt);
+          recurse(finished, remaining);
+        });
+      }
+    }
+
+    recurse([], optionsPackets);
+  };
+
+  const childOpts = [];
+  // Array.from(Array(self.treeSize[self.treePosView] || 0).keys()).forEach(function (i) {
+  //   childOpts.push({ module_path: self.pathname, pos: self.treePosView.concat([i]) });
+  // });
+
+  getRendered([mainOpts].concat(childOpts), cb);
+};
+
+
+const treeFromPackager = (mainOpts, cb) => {
+  execute(treeCommand(mainOpts), (output) => {
+    const json = JSON.parse(output.trim());
+    cb(json);
+  });
+};
+
+module.exports = {
+  convertFromCsg,
+  renderedFromPackager,
+  treeFromPackager,
+};
